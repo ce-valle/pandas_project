b56454e2b952aebbf45ed6829417e5998f03c7d5
ce-valle <zevalle@gmail.com>
2022-01-25 11:27:47 +0100

    en el análisis

---
+++ src/functions.py
@@ -0,0 +1,10 @@
+def columns_name(arr): #función para crear un diccionario y cambiar el nombre de las columnas
+    dict_columns = {columna: columna.replace(" ", "_").lower().strip() for columna in arr}
+    return dict_columns
+
+def cambio(col):
+    month_lst = ["Jan", "Feb", "Mar", "May", "Apr", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"]
+    if col not in month_lst:
+        return "unknown"
+    else:
+        return col 
